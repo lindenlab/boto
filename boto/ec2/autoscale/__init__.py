c57416814b1e20e320eea939daf56abf6da76e9b
Michael Kania <dynamike@lindenlab.com>
2012-09-05 17:34:18 +0000

    * switch autoscale back to hmac-v4

--- boto/ec2/autoscale/__init__.py
+++ boto/ec2/autoscale/__init__.py
@@ -119,7 +119,7 @@ class AutoScaleConnection(AWSQueryConnection):
                                     security_token=security_token)
 
     def _required_auth_capability(self):
-        return ['ec2']
+        return ['hmac-v4']
 
     def build_list_params(self, params, items, label):
         """
